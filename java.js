496d96ae2279c7e113d5b9b01d1a85874b2b3869
Mindaugas <mindaugasgrybauskas@protonmail.com>
2022-12-29 17:07:28 +0100

    Change custom font, create a function to check matching passwords

---
+++ java.js
@@ -0,0 +1,20 @@
+confirmPassword();
+
+function confirmPassword() {
+    const password = document.getElementById('password').value;
+    const password2 = document.getElementById('confirm-password').value;
+    let passwordStatus = document.getElementById('password-status');
+   
+    if (password === '') {
+        passwordStatus.style = 'color: red';
+        passwordStatus.textContent = 'Please enter matching passwords';
+    } else if (password === password2) {
+        document.getElementById('confirm-password').style.borderColor = 'green';
+        document.getElementById('password').style.borderColor = 'green';
+        passwordStatus.style = 'color: green';
+        passwordStatus.textContent = 'Passwords match';
+    } else {
+        passwordStatus.style = 'color: red';
+        passwordStatus.textContent = 'Passwords do not match';
+    }
+}
